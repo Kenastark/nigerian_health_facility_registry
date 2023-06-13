660126d134a4db27491d2d8cb8c8067a4373cb01
kenastark <ikennaudeani@gmail.com>
2023-06-13 17:46:32 +0100

    ran into a bottleneck, trying SQL

--- validate_facility.py
+++ validate_facility.py
@@ -1,43 +1,40 @@
 import difflib
 import pandas as pd
 import numpy as np
+import difflib as dl
 
 
-df_master = pd.read_csv("./HFR_with_coordinates_test.csv")
-df_hub = pd.read_csv("_link to hubs csv formated as state, LGA, facility name")
+df_master = pd.read_csv("./HFR_with_coordinates.csv")
+df_hub = pd.read_csv("Complied_hubs_from_IHVN_and_TLMN.csv")
 
-def match_and_correct_name(name, stage, lga, master_list):
-    # Filter master list by stage and LGA
-    filtered_list = [facility for facility in master_list if facility['stage'] == stage and facility['lga'] == lga]
+# Create an empty list to store the corrected facility names
+corrected_names = []
 
-    # Check for exact name match
-    exact_match = [facility for facility in filtered_list if facility['name'] == name]
-    if exact_match:
-        return exact_match[0]['name']  # Return the exact match
+# Loop through each facility_name record
+for name in df_hub['facility_name']:
+    hub_state = df_hub['state']
+    
+    for state in df_hub["state"]:
+        # Filter the master dataset based on the state
+        filtered_df = df_master.filter(regex='state', axis=1)
 
-    # Find closest name match based on percentage accuracy
-    best_match = difflib.get_close_matches(name, [facility['name'] for facility in filtered_list], n=1, cutoff=0.8)
-    if best_match:
-        return best_match[0]  # Return the best match
+         # Check if any match is found
+        if name == df_master.items('facility_name'):
+            corrected_name = filtered_df.iloc[0]['facility_name']
+            corrected_names.append(corrected_name)
+            
 
-    # If no match found, return None
-    return None
+        # Find closest name match based on percentage accuracy
+        best_match = difflib.get_close_matches(name, [facility['name'] for facility in filtered_df], n=1, cutoff=0.8)
+        if best_match:
+            corrected_names.append(best_match[0])  # Return the best match
+        else:
+            corrected_names.append("not_found")
+        
 
-# Example usage
-master_list = [
-    {'name': 'Facility A', 'stage': 'Stage 1', 'lga': 'LGA 1'},
-    {'name': 'Facility B', 'stage': 'Stage 2', 'lga': 'LGA 2'},
-    {'name': 'Facility C', 'stage': 'Stage 1', 'lga': 'LGA 2'},
-    {'name': 'Facility D', 'stage': 'Stage 3', 'lga': 'LGA 3'}
-]
+# Add the corrected names to a new column in the original DataFrame
+df_hub['corrected_facility_name'] = corrected_names
 
-name = 'Fecility A'
-stage = 'Stage 1'
-lga = 'LGA 1'
-
-corrected_name = match_and_correct_name(name, stage, lga, master_list)
-if corrected_name:
-    print(f"Match found: {corrected_name}")
-else:
-    print("No match found.")
+# Save the updated DataFrame to a new CSV file
+df_hub.to_csv('./updated_dataset.csv', index=False)
 
