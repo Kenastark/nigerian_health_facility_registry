ffb627c6a20cc508e2e93478bca20e2081d1ece2
kenastark <ikennaudeani@gmail.com>
2023-06-13 21:31:09 +0100

    fixed the SQL bug, code runs well

--- validate_facility.py
+++ validate_facility.py
@@ -1,33 +1,33 @@
 import difflib
 import pandas as pd
 import duckdb
-import difflib as dl
 
 
-df_master = pd.read_csv("Complied_hubs_from_IHVN_and_TLMN.csv")
+
+df_master = pd.read_csv("HFR_with_coordinates.csv")
 df_hub = pd.read_csv("Complied_hubs_from_IHVN_and_TLMN.csv")
 
 # Create an empty list to store the corrected facility names
 corrected_names = []
 
-for name in df_hub['facility_name']:
+for name, state in zip(df_hub['facility_name'], df_master['state']):
+    # Filter the master DataFrame by state
+    filter_master = df_hub[df_hub['state'] == state]
+    
+    if name in filter_master['facility_name'].values:
+        corrected_names.append(name)
 
-    for state in df_hub["state"]:
-
-        #filter the master list by state
-        filter_master = duckdb.query (
-        "SELECT * FROM df_master WHERE state =" + '"' + state + '"').to_df()
-
-        #comparing facility name in hub to facility name in master
-        for master_name in filter_master:
-            if name == master_name:
-                
-                corrected_names.append(name)
-            else: #looking for best match
-                best_match = difflib.get_close_matches(name,filter_master["facility_name"],n=1,cutoff=0.8)
-                corrected_names.append(best_match)
-            
+    else: #looking for best match
+        best_match = difflib.get_close_matches(name,filter_master["facility_name"],n=1,cutoff=0.6) #from 0.6 below the matching becomes less accurate
+        #from 0.65 it matches any "General Hospital" to "General Hospital Ohafia"
+        # at 0.7 it still matches to "General Hospital Ohafia"
+        # at 0.8 it matches a few(3) to "General Hospital Ohafia"
+        # at 0.85 it becomes accurate
+        if best_match:
+            corrected_names.append(best_match[0])
+        else:
             corrected_names.append("not_found")
+
 
 # Add the corrected names to a new column in the original DataFrame
 df_hub['corrected_facility_name'] = corrected_names
